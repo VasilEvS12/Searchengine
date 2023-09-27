fb45bebe154f92c27501a3ee4dcdb3aee7be8581
Evgeny Vasiliev <vasiliev@bk.ru>
2023-09-27 18:11:49 +0300

    Итоговый проект: Поисковый движок

--- src/main/java/searchengine/services/SearchingService.java
+++ src/main/java/searchengine/services/SearchingService.java
@@ -1,7 +1,6 @@
 package searchengine.services;
 
 import searchengine.dto.statistics.Response;
-import searchengine.dto.statistics.SearchingResponse;
 
 public interface SearchingService {
     Response search(String query, String site, int offset, int limit);
